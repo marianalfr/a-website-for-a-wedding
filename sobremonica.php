1de83a609c92fae2c24f730c2b6627a72b41d1ee
marianalfr <marianalfr@gmail.com>
2019-12-27 15:20:53 +0100

    Initial commit

---
+++ sobremonica.php
@@ -0,0 +1,26 @@
+<?php 
+if(isset($_POST['submit'])){
+    $to = "sobremonica@monicayboris.com"; // this is your Email address
+    
+    $name = $_POST['nombre'];
+    $sobreboris = $_POST['sobremonica'];
+
+    $subject = "SOBRE MÓNICA";
+    //$subject2 = "Copy of your form submission";
+    $message = "NOMBRE: ". $name . "\n\n";
+    $message .= "COMENTARIO:\n\n ". $sobreboris . "\n\n";
+   
+
+    //$message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+
+	$headers = "From:" . $from;
+	//$headers2 = "From:" . $to;
+  	mail($to,$subject,$message);
+    //mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    header('Location: recibido.php');//"Muchas gracias por tu confirmación, " . $first_name . ". Recuerda que puedes volver a usar este formulario si tus circunstancias cambian.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
+?>
+
+
+
